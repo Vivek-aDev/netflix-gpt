e50e1dd502359079139e231bc689360e04e8e23a
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-09-16 02:50:19 +0530

    add logo and background img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import netflixLogo from "../../assets/Netflix-logo.png";
+
 const Header = () => {
   return (
-    <>
-      <h3>header</h3>
-    </>
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+      <img className="w-60" src={netflixLogo} alt="logo" />
+    </div>
   );
 };
 export default Header;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Header from "../Header/Header";
+
+import netflixBg from "../../assets/Netflix-bg.jpg";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Header />
+      <img src={netflixBg} alt="background-img" />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
