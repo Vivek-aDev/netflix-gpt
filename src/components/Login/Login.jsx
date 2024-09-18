0300229cd67f2f7d15b0982c640f587330ba0f93
Vivekananda <vivekananda.work2022@gmail.com>
2024-09-18 23:15:30 +0530

    add validation for the input fields (#3)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "../Header/Header";
 import netflixBg from "../../assets/Netflix-bg.jpg";
+import { checkValidData } from "../../utils/validate";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  function handleButtonClick() {
+    // Validate the form data
+
+    // console.log(email.current.value);
+    // console.log(password);
+
+    const message = checkValidData(email.current.value, password.current.value);
+    console.log(message);
+    setErrorMessage(message);
+  }
 
   function handleToggle() {
     setIsSignIn(!isSignIn);
@@ -25,30 +42,40 @@ const Login = () => {
 
       {/* Login Form */}
       <div className="relative flex justify-center items-center h-full">
-        <form className="bg-black bg-opacity-60 w-full max-w-md p-10 rounded-md text-white shadow-lg">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="bg-black bg-opacity-60 w-full max-w-md p-10 rounded-md text-white shadow-lg"
+        >
           <h1 className="text-3xl font-bold mb-6">
             {isSignIn ? "Sign In" : "Sign Up"}
           </h1>
           <div className="space-y-4">
             {!isSignIn && (
               <input
+                ref={name}
                 className="border border-gray-600 bg-black bg-opacity-60 text-white p-3 rounded-sm w-full focus:outline-none focus:border-red-500"
                 type="text"
                 placeholder="Name"
               />
             )}
             <input
+              ref={email}
               className="border border-gray-600 bg-black bg-opacity-60 text-white p-3 rounded-sm w-full focus:outline-none focus:border-red-500"
               type="text"
               placeholder="Email or mobile number"
             />
             <input
+              ref={password}
               className="border border-gray-600 bg-black bg-opacity-60 text-white p-3 rounded-sm w-full focus:outline-none focus:border-red-500"
               type="password"
               placeholder="Password"
             />
           </div>
-          <button className="bg-red-600 hover:bg-red-700 p-3 mt-6 w-full font-semibold rounded-sm">
+          <p className="text-red-500 font-bold text-l py-2">{errorMessage}</p>
+          <button
+            className="bg-red-600 hover:bg-red-700 p-3 mt-6 w-full font-semibold rounded-sm"
+            onClick={handleButtonClick}
+          >
             {isSignIn ? "Sign In" : "Sign Up"}
           </button>
           <div className="flex justify-between items-center mt-4 text-gray-400 text-sm">
@@ -59,13 +86,12 @@ const Login = () => {
           </div>
           <div className="mt-8 text-gray-400">
             <span>{isSignIn ? "New to Netflix?" : "Already an User"} </span>
-            <a
-              href
+            <button
               className="text-white hover:underline cursor-pointer"
               onClick={handleToggle}
             >
               {isSignIn ? "Sign Up now." : "Sign In."}
-            </a>
+            </button>
           </div>
         </form>
       </div>
