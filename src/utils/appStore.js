0960167154bab51da5b1f48bb52864aeace66b6a
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-10-01 15:54:08 +0530

    redux setup

---
+++ src/utils/appStore.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "react";
+
+const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export default appStore;
