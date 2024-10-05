03ea931c278f7d403b832dca2a913ef66b5aad05
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-10-05 05:53:55 +0530

    fix sign out

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -26,11 +26,11 @@ const Body = () => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
-        const { uid, email, displayName } = user.uid;
+        const { uid, email, displayName } = user;
         dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
       } else {
         // User is signed out
-        dispatch(removeUser);
+        dispatch(removeUser());
       }
     });
   }, []);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,12 @@ import netflixLogo from "../../assets/Netflix-logo.png";
 import userLogo from "../../assets/user.png";
 import { auth } from "../../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const user = useSelector((store) => store.user);
+  // console.log(user);
+  
   const navigate = useNavigate();
   const handleSignOut = () => {
     signOut(auth)
@@ -23,15 +27,17 @@ const Header = () => {
       {/* <button className="bg-red-600 px-6 py-2 my-8 rounded-lg text-white font-semibold">
         Sign In
       </button> */}
-      <div className="flex p-5 justify-between">
-        <img className="w-12 h-12" src={userLogo} alt="userLogo" />
-        <button
-          onClick={handleSignOut}
-          className="font-bold text-white h-12 px-2"
-        >
-          (Sign Out)
-        </button>
-      </div>
+      {user && (
+        <div className="flex p-5 justify-between">
+          <img className="w-12 h-12" src={userLogo} alt="userLogo" />
+          <button
+            onClick={handleSignOut}
+            className="font-bold text-white h-12 px-2"
+          >
+            (Sign Out)
+          </button>
+        </div>
+      )}
     </div>
   );
 };
