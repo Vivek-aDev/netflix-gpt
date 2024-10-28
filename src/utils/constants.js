ed71bf2dbf947b32c761afb87abbed1f3be04fde
Vivekananda <vivekananda.work2022@gmail.com>
2024-10-28 12:11:52 +0530

    Get data from TMDB now playing movies list API (#10)

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGVkMWZhNWEzOGY2MjA2M2VmOWE5NjBmY2RjNzI1NSIsIm5iZiI6MTczMDA5MjE3My44MDI2ODksInN1YiI6IjY3MTcyNmI2YTc3YWYxZGJiYWY4YTZjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0LludM1PmC3eA167EZqei8QRSAD7ppYnpoqIKXpbTE4'
+    }
+  };
