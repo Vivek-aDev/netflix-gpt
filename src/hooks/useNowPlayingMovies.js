2a41a8bc588ce45d32451c7dce3b0f100fbedf4d
Vivekananda <vivekananda.work2022@gmail.com>
2024-11-27 10:23:35 +0530

    fix the data fetching issue by updating the fetch method (#13)

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -9,7 +9,7 @@ const useNowPlayingMovies = () => {
   const getNowPlayingMovies = async () => {
     const data = await fetch(NOW_PLAYING_MOVIES_URL, API_OPTIONS);
     const json = await data.json();
-    // console.log(json.results);
+    console.log(json.results);
     dispatch(addNowPlayingMovies(json.results));
   };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,10 @@
 export const NOW_PLAYING_MOVIES_URL =
-  "https://api.themoviedb.org/3/movie/now_playing?page=1";
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
 
 export const API_OPTIONS = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGVkMWZhNWEzOGY2MjA2M2VmOWE5NjBmY2RjNzI1NSIsIm5iZiI6MTczMDA5MjE3My44MDI2ODksInN1YiI6IjY3MTcyNmI2YTc3YWYxZGJiYWY4YTZjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0LludM1PmC3eA167EZqei8QRSAD7ppYnpoqIKXpbTE4",
-  },
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGVkMWZhNWEzOGY2MjA2M2VmOWE5NjBmY2RjNzI1NSIsIm5iZiI6MTczMjY4MjcwMC44MzE5MTkyLCJzdWIiOiI2NzE3MjZiNmE3N2FmMWRiYmFmOGE2YzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.h2ePTDASZC8nztVtZArMsUNMgNSIUyHPM9zzrv-FnLs'
+  }
 };
