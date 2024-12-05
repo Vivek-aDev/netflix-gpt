d51fc98f893e42561446409d6260cabdadda7b40
Vivek-aDev <vivekananda.work2022@gmail.com>
2024-12-06 00:55:50 +0530

    style background video and autoplay and mute

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { FaPlay } from "react-icons/fa";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black ">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/3">{overview}</p>
-      <div>
-        <button className="bg-gray-600 px-12 text-white py-4 rounded-md text-xl bg-opacity-50">
-          ▶️Play
+      <div className="flex items-center">
+        <button className="flex items-center justify-center gap-2 bg-white text-black hover:bg-gray-100 transition duration-300 shadow-md px-12 py-4 rounded-md text-xl font-semibold">
+          <FaPlay className="text-black" />
+          Play
         </button>
-        <button className="mx-2 bg-gray-600 px-12 text-white py-4 rounded-md text-xl bg-opacity-50">
+        <button className="mx-2 bg-gray-600 text-white hover:bg-gray-700 transition duration-300 shadow-md px-12 py-4 rounded-md text-xl font-semibold bg-opacity-80">
           More Info
         </button>
       </div>
